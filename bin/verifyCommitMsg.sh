50c4262e5080673791dda555e16c914e5ade8d52
v_chenqi <v_chenqi@bilibili.com>
2023-12-27 19:36:49 +0800

    feat:msg路径

--- bin/verifyCommitMsg.sh
+++ bin/verifyCommitMsg.sh
@@ -10,7 +10,7 @@ commitRE='^(merge|feat|fix|docs|styl|style|refactor|test|chore|revert):\s*[^\s].
 if echo "$msg" | grep -qE "$commitRE"; 
 then
     echo "$DONE commit信息校验成功";
-    source $(pwd)/bin/verifyCommitFile.sh;
+    source $(pwd)/node_modules/@chenqi_uu/uu-cli-git-hooks/bin/verifyCommitFile.sh;
 else 
     echo "commit message不符合规范。请修改为符合规范后再次提交。\n规则：<type>: <subject>\nsubject至少为4个字符（空格不参与），且不可为纯数字"
     exit 1;
